bf042449d7844e90e3113a2562f71a7492e92e58
Petr Janousek <pjanousek@lekis.cz>
2019-04-05 15:29:44 +0200

    Restoring original resources

--- SledovaniTVPlayer/SledovaniTVPlayer.Android/Resources/Resource.designer.cs
+++ SledovaniTVPlayer/SledovaniTVPlayer.Android/Resources/Resource.designer.cs
@@ -5998,7 +5998,7 @@ namespace SledovaniTVPlayer.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Icon = 2130903040;
+			public const int icon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
 			public const int icon_round = 2130903041;
