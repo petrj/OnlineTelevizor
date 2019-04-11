207d74e1271cc1bf96300af99a0c88b846a13dc2
Petr Janousek <pjanousek@lekis.cz>
2019-04-11 15:24:18 +0200

    Icons

---
+++ ResizeIconImage.ps1
@@ -0,0 +1,10 @@
+﻿cd $PSScriptRoot
+
+$convert = "C:\Program Files (x86)\ImageMagick\convert.exe"
+$ResourcesFolder = "SledovaniTVPlayer\SledovaniTVPlayer.Android\Resources"
+$SourceImage = "Icon.png"  # image in mipmap-xxxhdpi folder (192x192)
+
+& $convert -size 144x144 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 144x144 $ResourcesFolder\mipmap-xxhdpi\$SourceImage
+& $convert -size 96x96   $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 96x96   $ResourcesFolder\mipmap-xhdpi\$SourceImage
+& $convert -size 72x72   $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 72x72   $ResourcesFolder\mipmap-hdpi\$SourceImage
+& $convert -size 48x48   $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 48x48   $ResourcesFolder\mipmap-mdpi\$SourceImage
---
+++ ResizeLauncherImage.ps1
@@ -0,0 +1,10 @@
+﻿cd $PSScriptRoot
+
+$convert = "C:\Program Files (x86)\ImageMagick\convert.exe"
+$ResourcesFolder = "SledovaniTVPlayer\SledovaniTVPlayer.Android\Resources"
+$SourceImage = "launcher_foreground.png"  # image in mipmap-xxxhdpi folder (432x432 with image cca 220x220 in center)
+
+& $convert -size 324x324 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 324x324 $ResourcesFolder\mipmap-xxhdpi\$SourceImage
+& $convert -size 216x216 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 216x216 $ResourcesFolder\mipmap-xhdpi\$SourceImage
+& $convert -size 162x162 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 162x162 $ResourcesFolder\mipmap-hdpi\$SourceImage
+& $convert -size 108x108 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 108x108 $ResourcesFolder\mipmap-mdpi\$SourceImage
