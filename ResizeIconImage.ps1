c2ed8619bedda48e15878af4783867ab990200fb
Petr Janousek <pjanousek@lekis.cz>
2019-04-12 08:55:51 +0200

    Icons

--- ResizeIconImage.ps1
+++ ResizeIconImage.ps1
@@ -6,7 +6,12 @@ $SourceImage = "Icon.png"  # image in mipmap-xxxhdpi folder (192x192)
 
 if ($SourceImage -eq "Icon.png")
 {
-    & $convert -size 144x144 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 128x128 $ResourcesFolder\drawable\SledovaniTVPlayer.png
+    Copy-Item -Path $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -Destination $ResourcesFolder\drawable-xxxhdpi\Icon.png
+    
+    & $convert -size 128x128 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 128x128 $ResourcesFolder\drawable\$SourceImage
+    & $convert -size 144x144 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 144x144 $ResourcesFolder\drawable-xxhdpi\$SourceImage
+    & $convert -size 96x96   $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 96x96   $ResourcesFolder\drawable-xhdpi\$SourceImage
+    & $convert -size 72x72   $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 72x72   $ResourcesFolder\drawable-hdpi\$SourceImage
 }
 
 & $convert -size 144x144 $ResourcesFolder\mipmap-xxxhdpi\$SourceImage -resize 144x144 $ResourcesFolder\mipmap-xxhdpi\$SourceImage
