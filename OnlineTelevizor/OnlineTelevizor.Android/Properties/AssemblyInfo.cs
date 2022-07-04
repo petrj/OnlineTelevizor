32e4b851f002605c4662f88fc9e2a8eda11f15e1
petrjanousek.net@gmail.com <pjanousek@lekis.cz>
2022-07-04 14:30:59 +0200

    ver 54

--- OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
+++ OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using Android.App;
 // by using the '*' as shown below:.
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("53.0.0.0")]
-[assembly: AssemblyFileVersion("53.0.0.0")]
+[assembly: AssemblyVersion("54.0.0.0")]
+[assembly: AssemblyFileVersion("54.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
