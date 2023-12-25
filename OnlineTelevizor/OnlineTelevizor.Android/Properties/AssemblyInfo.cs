245f981c21d0050a309448383b1186722904fd78
Petr Janoušek <petrjanousek.net@gmail.com>
2023-12-25 13:44:27 +0100

    ver 71 - release build fix

--- OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
+++ OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using Android.App;
 // by using the '*' as shown below:.
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("69.0.0.0")]
-[assembly: AssemblyFileVersion("69.0.0.0")]
+[assembly: AssemblyVersion("71.0.0.0")]
+[assembly: AssemblyFileVersion("71.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
--- OnlineTelevizor/OnlineTelevizor.Android/SplashActivity.cs
+++ OnlineTelevizor/OnlineTelevizor.Android/SplashActivity.cs
@@ -8,7 +8,7 @@ using Android.Util;
 namespace OnlineTelevizor.Droid
 {
     [Activity(Name= "net.petrjanousek.OnlineTelevizor.SplashActivity", Label = "Online Televizor", Theme = "@style/SplashTheme", Banner = "@drawable/banner", MainLauncher = true, NoHistory = true, Exported = true)]
-    [IntentFilter(new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLauncher, Intent.CategoryLeanbackLauncher })]
+    [IntentFilter(new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLauncher })]
     public class SplashActivity : AppCompatActivity
     {
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
