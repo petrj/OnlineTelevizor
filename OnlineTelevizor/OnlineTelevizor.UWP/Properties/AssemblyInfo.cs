5660e3ef1e895efa9e851725e5f1c4b30c302afe
petrj <petrjanousek.net@gmail.com>
2024-07-11 23:07:50 +0200

    ver 74, updating nugets, target framework 34

--- OnlineTelevizor/OnlineTelevizor.UWP/Properties/AssemblyInfo.cs
+++ OnlineTelevizor/OnlineTelevizor.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Petr Janousek")]
 [assembly: AssemblyProduct("OnlineTelevizor.UWP")]
-[assembly: AssemblyCopyright("2019-2023")]
+[assembly: AssemblyCopyright("2019-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("72.0.0.0")]
-[assembly: AssemblyFileVersion("72.0.0.0")]
+[assembly: AssemblyVersion("74.0.0.0")]
+[assembly: AssemblyFileVersion("74.0.0.0")]
 [assembly: ComVisible(false)]
