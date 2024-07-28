3caa15b1ec3b2213f9cbaf311ce8aff85622a859
petrj <petrjanousek.net@gmail.com>
2024-07-28 13:04:26 +0200

    ver 79 for AndroidTV

--- OnlineTelevizor/OnlineTelevizor.UWP/Properties/AssemblyInfo.cs
+++ OnlineTelevizor/OnlineTelevizor.UWP/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("77.0.0.0")]
-[assembly: AssemblyFileVersion("77.0.0.0")]
+[assembly: AssemblyVersion("79.0.0.0")]
+[assembly: AssemblyFileVersion("79.0.0.0")]
 [assembly: ComVisible(false)]
