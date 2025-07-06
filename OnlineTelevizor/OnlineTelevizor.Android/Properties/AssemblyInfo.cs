7e2dba6bd27d838b11ada6ea1c3d4a4d5bcf196a
petrj <petrjanousek.net@gmail.com>
2025-07-06 03:59:04 +0200

    ver 84 apk release for Google Play

--- OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
+++ OnlineTelevizor/OnlineTelevizor.Android/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("82.0.0")]
-[assembly: AssemblyFileVersion("82.0.0.0")]
+[assembly: AssemblyVersion("84.0.0")]
+[assembly: AssemblyFileVersion("84.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
